f87ba556887b6f4c68d9d4562958c4b1ed53bd32
Sultanbek Asakeev <sultanbek.asakeev@gmail.com>
2025-07-07 11:44:22 +0600

    Revert "Version 2"
    This reverts commit 747f3f4931707f0c242c71bb654b99943ecf38cd.

--- src/main/java/com/autobots/java/weekdays/Version.java
+++ src/main/java/com/autobots/java/weekdays/Version.java
@@ -3,6 +3,5 @@ package com.autobots.java.weekdays;
 public class Version {
     public static void main(String[] args) {
         System.out.println("Verion 1.0.0");
-        System.out.println("Verion 1.0.2");
     }
 }
