231435725e5385982e7f794bfc0095b465d0cb44
Sultanbek Asakeev <sultanbek.asakeev@gmail.com>
2025-07-07 10:22:00 +0600

    Payment

---
+++ src/main/java/com/autobots/java/payment/VisaPayment.java
@@ -0,0 +1,11 @@
+package com.autobots.java.payment;
+
+public class VisaPayment {
+    public static void main(String[] args) {
+
+    }
+
+    public void payByVisaCard(double amount){
+        System.out.println("Visa payment by visa card");
+    }
+}
