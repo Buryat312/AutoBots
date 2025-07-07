7331972e9dc0cefc84d611daf08b95e5fc01b013
Sultanbek Asakeev <sultanbek.asakeev@gmail.com>
2025-07-07 11:41:30 +0600

    Revert "Version 3"
    This reverts commit 9c058029e7403ba45415dbbb1a2213e7d7b59b1c.

--- src/main/java/com/autobots/java/weekdays/Version.java
+++ src/main/java/com/autobots/java/weekdays/Version.java
@@ -4,6 +4,5 @@ public class Version {
     public static void main(String[] args) {
         System.out.println("Verion 1.0.0");
         System.out.println("Verion 1.0.2");
-        System.out.println("Verion 1.0.3----bug");
     }
 }
