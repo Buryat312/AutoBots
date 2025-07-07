747f3f4931707f0c242c71bb654b99943ecf38cd
Sultanbek Asakeev <sultanbek.asakeev@gmail.com>
2025-07-07 11:38:59 +0600

    Version 2

--- src/main/java/com/autobots/java/weekdays/Version.java
+++ src/main/java/com/autobots/java/weekdays/Version.java
@@ -3,5 +3,6 @@ package com.autobots.java.weekdays;
 public class Version {
     public static void main(String[] args) {
         System.out.println("Verion 1.0.0");
+        System.out.println("Verion 1.0.2");
     }
 }
